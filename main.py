dfeeea0f2cf49d7bed994a725f3ff77760d64e7c
WBS-MichaelMeyer <105566079+WBS-MichaelMeyer@users.noreply.github.com>
2022-11-19 12:21:22 +0100

    Empty App with test plot

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-# import librarys
-import streamlit as st
-import pandas as pd
+# import
+import streamlit as st # for web app
+import pandas as pd  # For data handling
+# visualisation
+import visualisation
+import matplotlib.pyplot as plt
+
 
 # import data
 url = 'https://drive.google.com/file/d/16EGubjXaeDwTH7sc6FnuQj7EI9ZfRYNL/view?usp=sharing'
 path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 df = pd.read_csv(path, sep = '\t')
 
+
+# APP Structure
 # header
 st.image("header.png")
 
 title = st.text_input('Enter one search term', 'Data Science')
 st.write('The current serach term is: ', title)
 
-# plot columns
 
+visualisation.test_plot()
+
+
+
+#tsnescatterplot(w2v_model, 'Kaufmann', [i[0] for i in w2v_model.most_similar(negative=["Kaufmann"])])
+
+# plot columns
 col1, col2 = st.columns(2)
 
 with col1:

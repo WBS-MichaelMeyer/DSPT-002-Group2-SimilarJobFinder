a25f6687ee9d5d03bf98556a6d78bbeb70b51f44
WBS-MichaelMeyer <105566079+WBS-MichaelMeyer@users.noreply.github.com>
2022-11-19 11:07:45 +0100

    Empty App

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+# import librarys
 import streamlit as st
 import pandas as pd
 
-st.write('Hi Team 2 ! :sunglasses:')
-
+# import data
 url = 'https://drive.google.com/file/d/16EGubjXaeDwTH7sc6FnuQj7EI9ZfRYNL/view?usp=sharing'
 path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 df = pd.read_csv(path, sep = '\t')
+
+# header
+st.image("header.png")
+
+title = st.text_input('Enter one search term', 'Data Science')
+st.write('The current serach term is: ', title)
+
+# plot columns
+
+col1, col2 = st.columns(2)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
 
 st.write(df)
